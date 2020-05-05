247b8f3cb0f69e5b18eaef73034e0e6e50cd102f
Cristian Murcia <cristiandmurcia@ucundinamarca.edu.co>
2020-05-05 17:19:55 -0500

    Cuarto commit

---
+++ tablas_de_multiplicar.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title> Tablas de multiplicar </title>
+</head>
+
+<body>	
+<?PHP
+for ($i = 1; $i <= 7; $i++){
+	for ($j = 1; $j <= 10; $j++){
+		echo $i," x ", $j , " = ", $i*$j, "<br>";
+	}
+	echo "<br>";
+}
+
+?>
+</body>
+</html>
